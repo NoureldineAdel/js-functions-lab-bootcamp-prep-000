5239d28f4bea16d5477b6e5c16f9203e18bb7839
Noureldine Adel <kevinblue947@gmail.com>
2019-05-28 14:13:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,5 +10,5 @@ function happyHolidaysTo(holiday, name){
   return `Happy ${holiday}, ${name}!`
 }
 function holidayCountdown(days, holiday) {
-  
+  return `It's ${days} days until ${holiday}!`
 }
